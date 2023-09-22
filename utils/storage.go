67eef7ab4dbb909686291d0aefc066a24d6766c3
cyfqyb <cy15770703229@163.com>
2023-09-23 04:15:30 +0800

    refactor: 完善图片压缩

--- utils/storage.go
+++ utils/storage.go
@@ -2,12 +2,14 @@ package utils
 
 import (
 	"Cgo/global"
+	"bytes"
 	"fmt"
 	"image"
 	_ "image/gif"
 	"image/jpeg"
 	_ "image/jpeg"
 	_ "image/png"
+	"io"
 	"mime/multipart"
 	"os"
 	"path"
@@ -17,17 +19,6 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-type multipartFileWrapper struct {
-	*os.File
-}
-
-func (w *multipartFileWrapper) Read(p []byte) (n int, err error) {
-	return w.File.Read(p)
-}
-
-func (w *multipartFileWrapper) Seek(offset int64, whence int) (int64, error) {
-	return w.File.Seek(offset, whence)
-}
 func UploadAliyunOss(file *multipart.FileHeader) string {
 	client, err := oss.New(
 		global.ConfigViper.GetString("aliyunOss.endpoint"),
@@ -37,18 +28,28 @@ func UploadAliyunOss(file *multipart.FileHeader) string {
 		global.Logger.Info(fmt.Sprintf("Failed to create OSS client, with error: %s", err.Error()))
 		os.Exit(-1)
 	}
+	src, err := file.Open()
+	// file, err = CompressImage(file)
 	// src, err := file.Open()
-	src, err := CompressImage(file)
 	if err != nil {
 		global.Logger.Info(fmt.Sprintf("Failed to open file, with error: %s", err.Error()))
 		os.Exit(-1)
 	}
-	defer src.Close()
+	defer func(src multipart.File) {
+		err := src.Close()
+		if err != nil {
+			return
+		}
+	}(src)
 	bucket, _ := client.Bucket(global.ConfigViper.GetString("aliyunOss.bucket"))
 	fileSuffix := path.Ext(file.Filename)
 	//拼接上传文件的名字并指定上传的路径
 	name := global.ConfigViper.GetString("aliyunOss.dir") + uuid.NewV4().String() + fileSuffix
-	err = bucket.PutObject(name, src)
+	imgData, err := CompressImage(src)
+	if err != nil {
+		global.Logger.Info(fmt.Sprintf("Failed to compress image, with error: %s", err.Error()))
+	}
+	err = bucket.PutObject(name, bytes.NewReader(imgData))
 	if err != nil {
 		global.Logger.Info(fmt.Sprintf("Failed to upload file, with error: %s", err.Error()))
 		os.Exit(-1)
@@ -58,34 +59,23 @@ func UploadAliyunOss(file *multipart.FileHeader) string {
 	return fmt.Sprintf("https://%s.%s/%s", global.ConfigViper.GetString("aliyunOss.bucket"), urlEndpoint, name)
 }
 
-func CompressImage(fileHeader *multipart.FileHeader) (multipart.File, error) {
-	file, err := fileHeader.Open()
-	if err != nil {
-		global.Logger.Info(fmt.Sprintf("Failed to open file, with error: %s", err.Error()))
-		os.Exit(-1)
-	}
-	defer file.Close()
+// 压缩图片的函数
+func CompressImage(file io.Reader) ([]byte, error) {
 	originalImage, _, err := image.Decode(file)
 	if err != nil {
 		return nil, err
 	}
-	// 创建一个临时文件来保存压缩后的图像
-	tempFile, err := os.CreateTemp("", "compressed_image_*.jpg")
-	if err != nil {
+
+	// 创建一个缓冲区来保存压缩后的图像
+	var compressedImageBuffer bytes.Buffer
+
+	// 压缩并保存图像到缓冲区
+	if err := jpeg.Encode(&compressedImageBuffer, originalImage, &jpeg.Options{Quality: global.ConfigViper.GetInt("image.quality")}); err != nil {
 		return nil, err
 	}
 
-	//压缩并保存图像到临时文件
-	if err = jpeg.Encode(tempFile, originalImage, &jpeg.Options{Quality: global.ConfigViper.GetInt("image.quality")}); err != nil {
-		return nil, err
-	}
-	// 创建一个模拟的*multipart.FileHeader
-	// compressedFileHeader := &multipart.FileHeader{
-	// 	Filename: fileHeader.Filename,
-	// 	Size:     os.Stat(tempFile),
-	// 	Header:   make(map[string][]string),
-	// }
-	return &multipartFileWrapper{
-		tempFile,
-	}, err
+	// 获取压缩后的图像数据
+	compressedImageData := compressedImageBuffer.Bytes()
+
+	return compressedImageData, nil
 }
